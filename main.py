21f0fef57b7ac7981844d8e53fef134b2018aa2c
sehandas <87594064+sehandas@users.noreply.github.com>
2021-07-23 20:45:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 import telebot
 
 
-bot = telebot.TeleBot("1902353429:AAEQluxyYFlU-jDmx11DT0-prZ2RgoYhQW4")
+   bot = telebot.TeleBot("1902353429:AAEQluxyYFlU-jDmx11DT0-prZ2RgoYhQW4")
 
-@bot.massage_handler(commands=["start"])
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
 bot.reply_to(massage,"hellow")
+
+@bot.message_handler(commands=["start"])
+def send_message(message):
+bot.send_message(massage,"hellow"
 
 bot.polling()
